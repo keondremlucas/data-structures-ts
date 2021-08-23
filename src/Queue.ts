de4a48c3ee2d5ed47fce49208fed2ad0fe3f2d12
Keondre <LTIUser@INFEDL31160.lntinfotech.com>
2021-08-23 16:25:54 -0400

    First Test Pass

--- src/Queue.ts
+++ src/Queue.ts
@@ -1,1 +1,53 @@
-export default class Queue {}
+export default class Queue {
+    
+    items: any[];
+    constructor(...params: any[]) 
+    {
+      console.log(params);
+      this.items = [...params];
+    }
+
+    enqueue(item: any)
+    {   
+        this.items.push(item);
+    }
+
+    dequeue() 
+    {
+        return this.items.shift();
+    } 
+    
+    getItems()
+    {
+          return this.items;
+    }
+   
+    getLast()
+    {   let a = this.items.length;
+        return this.items[a -1];
+    }
+    isEmpty() {
+
+        if(this.items[0] == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+            
+         
+    }
+    getFront()
+    {   if(this.isEmpty() == false)
+        {
+            return this.items[0] ;
+        }
+        else
+        {
+            return;
+        }
+
+    }
+}
